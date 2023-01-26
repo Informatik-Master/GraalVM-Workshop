53f6402e80ad6c0f2837fb50fd46f36f9849cd39
Matthias Vonend <matthias.vonend@outlook.de>
2023-01-26 16:20:53 +0100

    changes to docker

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get install -y git unzip build-essential libz-dev zlib1g-dev
-
-cd /workspaces/graal-workshop && wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.1/graalvm-ce-java19-linux-amd64-22.3.1.tar.gz 
-cd /workspaces/graal-workshop && tar -xvzf graalvm-ce-java19-linux-amd64-22.3.1.tar.gz
-
-
